e6a05ae18b5226ba2fd15725b4713fe1821f1fd9
Backflipper666 <elderly619@gmail.com>
2023-07-01 17:10:23 +0600

    allow cors from everywhere

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use('/', indexRouter);
 app.use('/api/shoes', shoesRouter);
 app.use('/api/user', userRouter);
